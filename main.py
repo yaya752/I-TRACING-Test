8234e558f93a0f8d24a9c4b7e733b8f22d58ae6c
yaya752 <yassine76440@gmail.com>
2025-05-08 15:01:17 +0200

    get issuing authority

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Logger import Logger
 import certstream
 import Levenshtein
 # Configuration
-OUR_DOMAIN = "pepito"
+OUR_DOMAIN = "admin"
 SIMILARITY_THRESHOLD = 0.75
 def similarity_check(domain1, domain2):
     """
@@ -15,13 +15,16 @@ def similarity_check(domain1, domain2):
 def my_callback(message, context): 
     #print(f"[LOG] {message}") 
     for domain in message['data']['leaf_cert']['all_domains']:
+        domain_is_typo = False
+        # Check if the domain is similar to OUR_DOMAIN
         similar_domains = []
         domains_split = domain.split('.') + domain.split('-') + domain.split('_')
         for word in domains_split:
             if similarity_check(word, OUR_DOMAIN) >= SIMILARITY_THRESHOLD:
-                similar_domains.append(domain)
-                print(f"[LOG] Similar domains found: {similar_domains},{similarity_check(word, OUR_DOMAIN)}")
-
+                domain_is_typo = True
+        if domain_is_typo:
+            # Log the suspicious domain
+            print(f"[LOG] Suspicious domain found: {domain} issuing authority: {message['data']['leaf_cert']['issuer']['aggregated']}")
 def on_open():
     print("Connection successfully established!")
 
